91dd4737908190c6ec1d924e6a3e82a4c4a39db3
will sherman <willsherman771@gmail.com>
2023-11-02 21:25:08 -0400

    added seed data

---
+++ server/config/seeds.js
@@ -0,0 +1,58 @@
+const seedData = {
+    users: [
+      {
+        username: "CaptainHook",
+        password: "piratepass123",
+        winStreak: 5,
+      },
+      {
+        username: "StealthNinja",
+        password: "ninjapass456",
+        winStreak: 3,
+      },
+      {
+        username: "Robotron",
+        password: "robotpass789",
+        winStreak: 7,
+      },
+      {
+        username: "SeaShark",
+        password: "sharkpass123",
+        winStreak: 2,
+      },
+      {
+        username: "ShadowBlade",
+        password: "bladepass456",
+        winStreak: 4,
+      },
+      {
+        username: "MetalGuardian",
+        password: "guardianpass789",
+        winStreak: 6,
+      },
+      {
+        username: "TreasureHunter",
+        password: "treasurepass123",
+        winStreak: 8,
+      },
+      {
+        username: "SilentAssassin",
+        password: "assassinpass456",
+        winStreak: 9,
+      },
+      {
+        username: "TechMaster",
+        password: "techpass789",
+        winStreak: 10,
+      },
+      {
+        username: "Buccaneer",
+        password: "buccaneerpass123",
+        winStreak: 7,
+      },
+    ],
+    // Add more data as needed
+  };
+  
+  module.exports = seedData;
+  
