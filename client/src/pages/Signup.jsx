d784ddcb0c912dced88a2dfc8bacf7a64dc1434e
will sherman <willsherman771@gmail.com>
2023-11-06 21:17:24 -0500

    starts at landing and links to signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import { ADDUSER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
@@ -12,7 +12,7 @@ const Signup = () => {
     email: '',
     password: '',
   });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser, { error, data }] = useMutation(ADDUSER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
