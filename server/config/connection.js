a28931f5b463f3b0a96d9340305b22fb46603e16
wjollie <william.jollie23@gmail.com>
2023-11-12 17:27:19 -0500

    s

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,23 +1,7 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/programming-thoughts",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/programming-thoughts"
 );
 
-// Get the default connection
-const db = mongoose.connection;
-
-// Event listener for the connection error
-db.on("error", (error) => {
-  console.error("MongoDB connection error:", error);
-});
-
-// Event listener for the successful connection
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Export the connection
-module.exports = db;
+module.exports = mongoose.connection;
